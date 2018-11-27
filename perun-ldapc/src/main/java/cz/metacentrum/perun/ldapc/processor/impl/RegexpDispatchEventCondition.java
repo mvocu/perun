3850f7b23269eb6f1a3ca985f307cec452c3f9e5
Michal Voců <michal.vocu@gmail.com>
2018-11-27 09:49:34 +0100

    add missing group creation event and regex

--- perun-ldapc/src/main/java/cz/metacentrum/perun/ldapc/processor/impl/RegexpDispatchEventCondition.java
+++ perun-ldapc/src/main/java/cz/metacentrum/perun/ldapc/processor/impl/RegexpDispatchEventCondition.java
@@ -13,7 +13,7 @@ public class RegexpDispatchEventCondition extends SimpleDispatchEventCondition {
 
 	@Required
 	public void setPattern(String regexp) {
-		this.pattern = Pattern.compile(regexp, Pattern.MULTILINE);
+		this.pattern = Pattern.compile(regexp, Pattern.MULTILINE | Pattern.DOTALL);
 	}
 
 	@Override
